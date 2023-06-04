6a8cad0ec2b2288e224f6401b5136c28424df384
LuanCalais <luancalais@gmail.com>
2023-06-04 15:34:43 -0300

    create nav if user not logged and install bootstrap and box icons by cdn

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,33 @@
-const Header = () => {
-  return <header>Header Works</header>;
+import styles from "./Header.module.css";
+
+const Header = ({ isLoged }) => {
+  if (isLoged) {
+    return <h1>Usuário logado</h1>;
+  }
+  return (
+    <nav>
+      <div className={styles.container}>
+        <a
+          href="https://www.instagram.com/luan_calais/"
+          target="_blank"
+          rel="noreferrer"
+          title="Luan Calais Instagram"
+          alt="Luan Calais Instagram"
+        >
+          <i className="bx bxl-instagram-alt"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/luan-s-calais-186104217/"
+          target="_blank"
+          rel="noreferrer"
+          title="Luan Calais Linkedin"
+          alt="Luan Calais Linkedin"
+        >
+          <i className="bx bxl-linkedin-square"></i>
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
