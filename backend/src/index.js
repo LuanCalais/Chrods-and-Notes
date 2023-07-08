6c1309ea5161a0c9e84a3728d241f5e067e3530e
LuanCalais <luancalais@gmail.com>
2023-07-08 16:11:58 -0300

    connect with mongoDb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,12 @@
 import express from "express";
 import Routes from "./routes/index.js";
+import db from "../config/dbConnect.js";
+
+db.on("error", console.log.bind(console, "Connection error :("));
+
+db.once("open", () => {
+    console.log('Connection success :)')
+})
 
 const app = express();
 const port = 3000;
