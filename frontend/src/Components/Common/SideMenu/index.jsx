c7d17f985684d532cb59538e67b8c2dd96d0e3e4
LuanCalais <luancalais@gmail.com>
2023-11-27 21:19:22 -0300

    init handle menu

--- frontend/src/Components/Common/SideMenu/index.jsx
+++ frontend/src/Components/Common/SideMenu/index.jsx
@@ -1,5 +1,28 @@
-const SideMenu = () => {
-  return <div>Side Menu Works</div>;
+import styles from "./SideMenu.module.css";
+
+const SideMenu = ({
+  items = [
+    { name: "Home", icon: "", isActive: true, action: () => {} },
+    { name: "Bands", icon: "", isActive: false, action: () => {} },
+    { name: "Musics", icon: "", isActive: false, action: () => {} },
+  ],
+}) => {
+  return (
+    <div className={styles.sidebarContainer}>
+      <ul className={styles.sidebaritemsMenu}>
+        {items.map((item, i) => {
+          return (
+            <li
+              className={item.isActive ? styles.active : ""}
+              key={`${item.name}_${i}`}
+            >
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default SideMenu;
