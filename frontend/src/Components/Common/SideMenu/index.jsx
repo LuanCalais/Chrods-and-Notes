a531e491d304dd0979e104a423fef25161a66676
LuanCalais <luancalais@gmail.com>
2023-11-30 12:40:08 -0300

    handle click selected item

--- frontend/src/Components/Common/SideMenu/index.jsx
+++ frontend/src/Components/Common/SideMenu/index.jsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import styles from "./SideMenu.module.css";
 
 const SideMenu = ({
   items = [
-    { name: "Home", icon: "cabin", isActive: true, action: () => {} },
-    { name: "Bands", icon: "diversity_3", isActive: false, action: () => {} },
-    { name: "Musics", icon: "nightlife", isActive: false, action: () => {} },
+    { name: "Home", icon: "cabin", action: () => {} },
+    { name: "Bands", icon: "diversity_3", action: () => {} },
+    { name: "Musics", icon: "nightlife", action: () => {} },
   ],
 }) => {
+  const [selectedItem, setSelectedItem] = useState(0);
+
+  function handleSelected(i) {
+    setSelectedItem(i);
+  }
+
   return (
     <div className={styles.sidebarContainer}>
       <ul className={styles.sidebaritemsMenu}>
         {items.map((item, i) => {
           return (
             <li
-              className={item.isActive ? styles.active : ""}
+              className={selectedItem === i ? styles.active : ""}
               key={`${item.name}_${i}`}
+              onClick={() => handleSelected(i)}
             >
-              <span class="material-icons">{item.icon}</span>
+              <span className="material-icons">{item.icon}</span>
               {item.name}
             </li>
           );
