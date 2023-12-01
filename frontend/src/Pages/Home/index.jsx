47f3c3f6fd52aca1003c0a6089dfc74c8e2452ed
LuanCalais <luancalais@gmail.com>
2023-12-01 17:03:49 -0300

    handle logged user in main component

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <div>Home content works</div>;
+import styles from "./Home.module.css";
+
+const Home = ({ user }) => {
+  return (
+    <div className={styles.contentContainer}>
+      <h2> Wellcome back, {user.name} :) </h2>
+    </div>
+  );
 };
 
 export default Home;
