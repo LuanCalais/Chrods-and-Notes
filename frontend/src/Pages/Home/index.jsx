f9d1e5b4ba39d0987f56243691e8cf0b634a306d
LuanCalais <luancalais@gmail.com>
2023-12-01 17:16:46 -0300

    apply disco image

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,9 +1,13 @@
 import styles from "./Home.module.css";
+import disco from "./disco.png";
 
 const Home = ({ user }) => {
   return (
     <div className={styles.contentContainer}>
-      <h2> Wellcome back, {user.name} :) </h2>
+      <div className={styles.topContent}>
+        <h2> Wellcome back, {user.name} :) </h2>
+        <img src={disco} alt="disco" />
+      </div>
     </div>
   );
 };
