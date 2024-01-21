aed55223296e3e5554512f979acbb40db699b644
LuanCalais <luancalais@gmail.com>
2024-01-21 13:13:57 -0300

    init create band modal

--- frontend/src/Pages/Bands/index.jsx
+++ frontend/src/Pages/Bands/index.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import styles from "./Bands.module.css";
 import Search from "../../Components/Search";
 import Button from "../../Components/Button";
+import Input from "../../Components/Common/CommonInput";
+import Modal from "../../Components/Common/CommonModal";
+import ModalButton from "../../Components/Common/Button";
 
 const Bands = () => {
   const [search, setSearch] = useState("");
+  const [show, setShow] = useState(false);
+  const [band, setBand] = useState();
 
   const searchObject = {
     label: "Add",
@@ -12,20 +17,49 @@ const Bands = () => {
       backgroundColor: "#012622",
       color: "#ECE5F0",
     },
-    action: addBand,
+    action: () => setShow(true),
   };
 
-  function addBand() {
-    alert("TODO: handle add band");
+  function createBand() {
+    alert("Criando banda");
   }
 
   return (
-    <div className={styles.bands}>
-      <div className={styles.inputs}>
-        <Search value={search} setValue={setSearch} />
-        <Button {...searchObject} />
+    <>
+      <div className={styles.bands}>
+        <div className={styles.inputs}>
+          <Search value={search} setValue={setSearch} />
+          <Button {...searchObject} />
+        </div>
       </div>
-    </div>
+      <Modal title="Create band" show={show}>
+        <Input placeholder="Name" handleValue={() => {}} type="text" />
+        <Input placeholder="Gender" handleValue={() => {}} type="text" />
+        <Input placeholder="Created Year" handleValue={() => {}} type="text" />
+        <div className={styles.buttons}>
+          <ModalButton
+            label="Criar"
+            color="var(--light-color)"
+            background="var(--deep-dark-green)"
+            width="50%"
+            height="41px"
+            fontSize="14px"
+            actionFunction={() => createBand}
+          />
+
+          <ModalButton
+            label="Voltar"
+            color="var(--deep-dark-green)"
+            background="var(--light-color)"
+            borderColor="var(--deep-dark-green)"
+            width="50%"
+            height="41px"
+            fontSize="14px"
+            actionFunction={() => setShow(false)}
+          />
+        </div>
+      </Modal>
+    </>
   );
 };
 
