02fb515d52d186dc0533f0f7d79670c52b548442
LuanCalais <luancalais@gmail.com>
2024-08-18 16:34:05 -0300

    refactor: change integer to constant

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -1,3 +1,19 @@
 export const CONNECTION_STRING = Object.freeze({
     MONGO_DB: "mongodb+srv://KingOfNothing:125678943@kingofnothing.huxpw63.mongodb.net/ChordsAndNotes"
 })
+
+export const PROFILE_PICTURE = Object.freeze({
+    DOG: {
+      name: "Dog",
+      code: "DOG",
+    },
+    CAT: {
+      name: "Cat",
+      code: "CAT",
+    },
+    MNK: {
+      name: "Monkey",
+      code: "MNK",
+    },
+  });
+  
--- backend/src/scripts/updateUserSchemaAddColumnPicture.js
+++ backend/src/scripts/updateUserSchemaAddColumnPicture.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import UserModel from "../models/UserModel.js";
-import { CONNECTION_STRING } from "../constants/index.js";
+import { CONNECTION_STRING, PROFILE_PICTURE } from "../constants/index.js";
 
 const updateUserSchemaAddColumnPicture = async () => {
   try {
@@ -8,7 +8,10 @@ const updateUserSchemaAddColumnPicture = async () => {
 
     const users = await UserModel.find();
 
-    await UserModel.updateMany({}, { $set: { profilePicture: 0 } });
+    await UserModel.updateMany(
+      {},
+      { $set: { profilePicture: PROFILE_PICTURE.DOG.code } }
+    );
     console.log(`Atualizado ${users.length} com a coluna 'profilePicture'`);
   } catch (ex) {
     console.log(`Aconteceu um erro ao executar o script ${ex}`);
